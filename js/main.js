6af45c751517e106422aa5681c9021b23f2dd5aa
zasipin <ZasypinNV@yanos.slavneft.ru>
2018-03-12 16:16:54 +0300

    fix: Add tabindex to restaurant card elements

--- js/main.js
+++ js/main.js
@@ -152,14 +152,17 @@ createRestaurantHTML = (restaurant) => {
 
   const name = document.createElement('h1');
   name.innerHTML = restaurant.name;
+  name.tabIndex = 0;
   li.append(name);
 
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
+  neighborhood.tabIndex = 0;
   li.append(neighborhood);
 
   const address = document.createElement('p');
   address.innerHTML = restaurant.address;
+  address.tabIndex = 0;
   li.append(address);
 
   const more = document.createElement('a');
