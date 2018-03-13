e141aedf4c54254af33fcda45b44ae440ebc4c12
zasipin <ZasypinNV@yanos.slavneft.ru>
2018-03-13 13:24:28 +0300

    fix: Add aria-label for address

--- js/common_functions.js
+++ js/common_functions.js
@@ -4,11 +4,12 @@
 const createAddressHTML = (restaurant) => {
   const address = document.createElement('p');
   address.innerHTML = restaurant.address;
-  address.classList.add("adr");
+  address.classList.add('adr');
+  address.setAttribute('aria-label', 'Restaurant address');
   address.tabIndex = 0;
   
   const div = document.createElement('div');
-  div.classList.add("vcard");
+  div.classList.add('vcard');
 
   div.append(address);
   return div;
