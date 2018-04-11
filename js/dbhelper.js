76791fdd434d6f8c02bb88345cce9d4307f8c361
zasipin <ZasypinNV@yanos.slavneft.ru>
2018-04-11 16:44:44 +0300

    feat: Read restaurants data from server

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -8,46 +8,39 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    const port = 8000 // Change this to your server port
-    return `http://localhost:${port}/data/restaurants.json`;
+    const port = 1337 // Change this to your server port
+    return `http://localhost:${port}/`;
   }
 
   /**
    * Fetch all restaurants.
    */
   static fetchRestaurants(callback) {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', DBHelper.DATABASE_URL);
-    xhr.onload = () => {
-      if (xhr.status === 200) { // Got a success response from server!
-        const json = JSON.parse(xhr.responseText);
-        const restaurants = json.restaurants;
+
+    fetch(`${DBHelper.DATABASE_URL}restaurants`)
+      .then((resp) => resp.json())
+      .then(restaurants => { 
+        console.log('fetchRestaurants called');
         callback(null, restaurants);
-      } else { // Oops!. Got an error from server.
-        const error = (`Request failed. Returned status of ${xhr.status}`);
+      })
+      .catch(err => {
+        const error = (`Request failed. Returned status of ${err.status}`);
         callback(error, null);
-      }
-    };
-    xhr.send();
+      });
   }
 
   /**
    * Fetch a restaurant by its ID.
    */
   static fetchRestaurantById(id, callback) {
-    // fetch all restaurants with proper error handling.
-    DBHelper.fetchRestaurants((error, restaurants) => {
-      if (error) {
-        callback(error, null);
-      } else {
-        const restaurant = restaurants.find(r => r.id == id);
-        if (restaurant) { // Got the restaurant
-          callback(null, restaurant);
-        } else { // Restaurant does not exist in the database
-          callback('Restaurant does not exist', null);
-        }
-      }
-    });
+    fetch(`${DBHelper.DATABASE_URL}restaurants/${id}`)
+      .then((resp) => resp.json())
+      .then(restaurant => { 
+        callback(null, restaurant);
+      })
+      .catch(err => {
+        callback('Restaurant does not exist', null);
+      });
   }
 
   /**
@@ -150,7 +143,7 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant, prefix = "") {
-    return (`/img/${prefix}${restaurant.photograph}`);
+    return (`/img/${prefix}${restaurant.photograph}.jpg`);
   }
 
   /**
