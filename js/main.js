4717bee3fa22caf13ed8450d6325b8d9ee31a3e2
zasipin <zasipin.it@gmail.com>
2018-04-25 21:43:51 +0300

    fix: Load img files asyncronously

--- js/main.js
+++ js/main.js
@@ -142,18 +142,28 @@ createRestaurantHTML = (restaurant) => {
   li.classList.add(["restaurant-item"]);
   li.setAttribute('aria-label', 'restaurant item');
 
-  const image = document.createElement('img');
-  image.className = 'restaurant-img';
-  //image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  image.alt = `${restaurant.name} restaurant photo`;
-  const imgSrc_266 = DBHelper.imageUrlForRestaurant(restaurant, "resized_266/"); 
-  const imgSrc_430 = DBHelper.imageUrlForRestaurant(restaurant, "resized_430/"); 
-  const imgSrc_600 = DBHelper.imageUrlForRestaurant(restaurant, "resized_600/"); 
-  const imgSrc_800 = DBHelper.imageUrlForRestaurant(restaurant);
-  image.src = imgSrc_430; 
-  image.srcset = `${imgSrc_266} 266w, ${imgSrc_430} 430w, ${imgSrc_600} 600w, ${imgSrc_800} 800w`;
-  image.sizes = '(max-width: 769px) 100vw, (max-width: 1039px) 50vw, (min-width: 1040px) 30vw';
-  li.append(image);
+  
+
+    const image = document.createElement('img');
+    let photograph = restaurant.photograph;
+    restaurant.photograph = undefined;
+    image.className = 'restaurant-img';
+    image.src = DBHelper.imageUrlForRestaurant(restaurant);
+    image.alt = `${restaurant.name} restaurant photo`;
+    
+    li.append(image);
+    restaurant.photograph = photograph;
+    setTimeout(()=>{
+      const imgSrc_266 = DBHelper.imageUrlForRestaurant(restaurant, "resized_266/"); 
+      const imgSrc_430 = DBHelper.imageUrlForRestaurant(restaurant, "resized_430/"); 
+      const imgSrc_600 = DBHelper.imageUrlForRestaurant(restaurant, "resized_600/"); 
+      const imgSrc_800 = DBHelper.imageUrlForRestaurant(restaurant);
+      image.src = imgSrc_430;
+      image.srcset = `${imgSrc_266} 266w, ${imgSrc_430} 430w, ${imgSrc_600} 600w, ${imgSrc_800} 800w`;
+      image.sizes = '(max-width: 769px) 100vw, (max-width: 1039px) 50vw, (min-width: 1040px) 30vw';
+    }, 300);
+
+
 
   const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
@@ -173,8 +183,8 @@ createRestaurantHTML = (restaurant) => {
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
-
+  li.append(more);
+ 
   return li;
 }
 
